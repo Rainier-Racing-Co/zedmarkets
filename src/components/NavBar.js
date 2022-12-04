c10b77b7ed558197b0a832642192db023ca0caa4
Harper Foley <harper.e.foley@gmail.com>
2022-12-04 03:01:03 +0000

    Makes NavBar dynamic with addition of nav.json file as reference file

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,28 +1,18 @@
 import React from 'react';
 import { Nav } from 'react-bootstrap';
+import navItems from '../data/nav.json';
 import '../css/NavBar.css';
-import { NavLink } from 'react-router-dom';
 
 class NavBar extends React.Component {
   render() {
+    let navbar = navItems.map((item, idx) => (
+      <a key={idx} href={item.url}>{item.display_name}</a>
+    ));
+
     return(
       <>
         <Nav activeKey="/" as="ul">
-          <Nav.Item>
-            <NavLink to="/">Home</NavLink>
-          </Nav.Item>
-          <Nav.Item>
-            <NavLink to="/markets">Live Markets</NavLink>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link href="https://rainierracingco.com">Rainier Racing Co.</Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link href="https://www.hawku.com/c/zed_run/zed_horse?selected_mode=list&selected_tab=listings">Hawku</Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link href="https://zed.run">ZED Run</Nav.Link>
-          </Nav.Item>
+          {navbar}
         </Nav>
       </>
     );

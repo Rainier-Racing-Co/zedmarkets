76827ebe62285e287372ceacb1007325982260e5
Harper Foley <harper.e.foley@gmail.com>
2023-01-06 23:13:10 -0800

    Adds Hawku query function and caches query in state

--- src/components/App.js
+++ src/components/App.js
@@ -15,10 +15,13 @@ class App extends React.Component {
     this.state = {
       horses: [],
       queriedHorseData: [],
-      queriedHorse: null
+      queriedHorse: null,
+      marketQueryStartDate: 1600000000,
+      marketQueryEndDate: Date.now()/1000, //units for api call required to be in seconds
+      marketDataCache: []
     };
   }
-  
+
   handleHorseQueryInput = async (e) => {
     await this.setState({
       queriedHorse: parseInt(e.target.value)
@@ -37,10 +40,28 @@ class App extends React.Component {
       console.log(err.message);
     }
   };
+
   handleHorseQuerySubmit = async (e) => {
     e.preventDefault();
     await this.getHorse(this.state.queriedHorse);
     console.log(this.state.horses);
+  };
+
+  handleMarketQuery = async() => {
+    try {
+      console.log(this.state.marketDataCache);
+      console.log(this.state.marketQueryStartDate);
+      console.log(this.state.marketQueryEndDate);
+
+      let marketURL = `${process.env.REACT_APP_SERVER_URL}/markets?start_date=${this.state.marketQueryStartDate}&end_date=${this.state.marketQueryEndDate}`;
+      let marketData = await axios.get(marketURL);
+
+      console.log('marketData', marketData.data);
+      this.setState({marketDataCache: marketData.data});
+      console.log(this.state.marketDataCache);
+    } catch(err) {
+      console.log(err.message);
+    }
   };
 
   render () {
@@ -50,7 +71,7 @@ class App extends React.Component {
         <div className='all-content-container'>
           <Routes>
             <Route exact path="/" element={<LandingPage/>}/>
-            <Route exact path="/markets" element={<LiveMarkets/>}/>
+            <Route exact path="/markets" element={<LiveMarkets handleMarketQuery={this.handleMarketQuery}/>}/>
             <Route exact path="/horse" element={<HorseLookup handleHorseQuerySubmit={this.handleHorseQuerySubmit} horse={this.state.queriedHorseData} queriedHorse={this.state.queriedHorse} handleHorseQueryInput={this.handleHorseQueryInput}/>}/>
             <Route exact path="*" />
           </Routes>
--- src/components/LiveMarkets.js
+++ src/components/LiveMarkets.js
@@ -9,6 +9,7 @@ class LiveMarkets extends React.Component {
         <div className="markets-headline">
           <h1 id="markets-title">Live Markets Page</h1>
         </div>
+        <button onClick={this.props.handleMarketQuery}>Get Market Data</button>
         <div className='chart'>
           <img src="https://via.placeholder.com/600x400?text=Chart+of+Market+Data" className="price-chart"/>
         </div>
